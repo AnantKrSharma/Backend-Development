19ece27a704323915c5915500f467b0a3e594ae5
Anant Kr Sharma <145072088+AnantKrSharma@users.noreply.github.com>
2024-03-31 05:22:13 +0530

    Add files via upload

---
+++ Course_Selling_App/Routes/admin.js
@@ -0,0 +1,81 @@
+const { Router } = require("express");
+const adminMiddleware = require("../middleware/admin");
+const router = Router();
+
+const {Admin, Course} = require('../db/index')
+
+const jwt = require('jsonwebtoken')
+const {jwtSecret} = require('../config')
+
+// Admin Routes
+router.post('/signup', (req, res) => {
+    // Implement admin signup logic
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    Admin.create({
+        username: username,
+        password: password
+    }).then(()=>{
+        res.json({
+            msg: "Admin created succesfully."
+        })
+    }).catch(()=>{
+        res.json({
+            msg:"Admin not created."
+        })
+    })
+});
+
+
+router.post('/signin', async (req, res) => {
+    // Implement admin signin logic
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    const isValidated = await Admin.findOne({username: username, password: password})
+    if(isValidated){
+        const Token = jwt.sign({username: username}, jwtSecret)
+        res.json({
+            Token
+        });
+    }
+    else{
+        res.status(403).json({
+            msg:"Incorrect username and password."
+        })
+    }
+});
+
+
+router.post('/courses', adminMiddleware, (req, res) => {
+    // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const price = req.body.price;
+    const imageLink = req.body.imageLink;
+
+    Course.create({
+        title: title,
+        description: description,
+        price: price,
+        imageLink: imageLink
+    }).then((newCourse)=>{
+        res.json({
+            msg: "Course created successfully",
+            courseId : newCourse._id
+        })
+    })
+});
+
+
+router.get('/courses', adminMiddleware, async (req, res) => {
+    // Implement fetching all courses logic
+    const allCourses = await Course.find({});
+    res.json({
+        allCourses
+    })
+});
+
+
+module.exports = router;
---
+++ Course_Selling_App/Routes/user.js
@@ -0,0 +1,102 @@
+const { Router } = require("express");
+const router = Router();
+const userMiddleware = require("../middleware/user");
+
+const { Course, User } = require("../db");
+
+const jwt = require('jsonwebtoken')
+const {jwtSecret} = require('../config')
+
+
+// User Routes
+router.post('/signup', async (req, res) => {
+    // Implement user signup logic
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    await User.create({
+        username: username,
+        password: password
+    }).then(()=>{
+        res.json({
+            msg: "Successfully created user."
+        })
+    }).catch(()=>{
+        res.json({
+            msg:"User not created."
+        })
+    })
+});
+
+
+router.post('/signin', (req, res) => {
+    // Implement admin signup logic
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    const isValidated = User.findOne({username: username, password: password})
+    
+    if(isValidated){
+        const token = jwt.sign({username: username}, jwtSecret)
+        res.json({
+            token
+        })
+    }
+    else{
+        res.json({
+            msg: "Incorrect username and password"
+        })
+    }
+    
+});
+
+
+router.get('/courses', async (req, res) => {
+    // Implement listing all courses logic
+    try{
+        const AllCourses = await Course.find({});
+        res.json({
+            AllCourses
+        })
+    }
+    catch{
+        res.json({
+            msg:"Error occured."
+        })
+    }
+});
+
+
+router.post('/courses/:courseId', userMiddleware, async (req, res) => {
+    // Implement course purchase logic
+    const username = req.username;
+    const courseID = req.params.courseId
+
+    const updated = await User.updateOne({username: username}, {
+        $push:{
+            purchasedCourses: courseID
+        }
+    })
+    res.json({
+        msg: "Succesfully purchased."
+    })
+});
+
+
+router.get('/purchasedCourses', userMiddleware,async (req, res) => {
+    // Implement fetching purchased courses logic
+    const username = req.username;
+    const userID =await User.findOne({username: username})
+    const coursesArr = userID.purchasedCourses;
+    const userCourses = await Course.find({
+        _id:{
+            $in: coursesArr
+        }
+    })
+    
+    res.json({
+        userCourses
+    })
+});
+
+module.exports = router
